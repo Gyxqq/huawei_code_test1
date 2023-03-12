a1b5fdc6336cbf40b28614ba2b204d0629dbb77d
Gyxqq <108503818+Gyxqq@users.noreply.github.com>
2023-03-12 20:27:03 +0800

    gyx-122027 机器人类架构调整，请对对应函数进行同构，增加机器人数据结构体，已放入头文件

--- code/data_struct.h
+++ code/data_struct.h
@@ -103,3 +103,15 @@ public:
         cin>>x>>y;
     }
 };
+struct robot_data{
+   int num;           // 机器人编号
+  double x, y;       // 机器人位置
+  int table;         // 机器人所处的工作台ID -1表示当前没有处于任何工作台周围 从0开始
+  int object;        // 携带物品种类 0表示未携带
+  double time_value; // 时间价值系数
+  double col_value;  // 碰撞价值系数
+  double ang_speed;  // 角速度
+      double speed_x,
+      speed_y;   // 线速度
+  double toward; // 朝向 与平面直角坐标系的单位圆射线方向相同 toward=0时向右
+};
