c7415ac2ec9486c831dea038b0a0b4cb946090c0
Gyxqq <108503818+Gyxqq@users.noreply.github.com>
2023-03-12 22:12:42 +0800

    增加架构图，main.cpp增加部分内容

--- code/main.cpp
+++ code/main.cpp
@@ -1,8 +1,12 @@
 //主函数
 #include<stdio.h>
+#include"c_robot.h"
+#include"c_map.h"
 int main(){
-  
-printf("");
+
+     map now_map;
+     robot* now_bot=new robot[4];
+     now_map.mapinit(now_bot); //地图初始化
 
   
 }
