9dbad97255ae1d77f1f1e8bc3bd52bf5ea697644
yca <321770977@qq.com>
2023-03-12 12:21:21 +0800

    yca-121221 创建data_struct.h头文件,创建input.cpp

---
+++ code/input.cpp
@@ -0,0 +1,7 @@
+#include"data_struct.h"
+#include<iostream>
+#include<cstdio>
+using namespace std;
+bool input(int& now_frame,int& now_money,table* table_in_map,int table_num,robot* robot_in_map){
+    
+}
