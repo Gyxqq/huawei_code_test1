2c7b94c621ba73982869029b5c68de4bd8d262a1
Gyxqq <108503818+Gyxqq@users.noreply.github.com>
2023-03-12 20:57:54 +0800

    gyx-122058 c_map,c_robot创建和部分实现

---
+++ code/c_map.cpp
@@ -0,0 +1,15 @@
+#include "c_map.h"
+#include "c_robot.h"
+bool map::mapinit(robot& bot)
+{
+   
+}
+table *map::gettable()
+{
+
+  return table_in_map;
+}
+int map::gettable_num()
+{
+  return table_num;
+}
---
+++ code/c_robot.h
@@ -0,0 +1,26 @@
+#include"data_struct.h"
+class robot
+{ // 机器人类
+
+
+ 
+
+public:
+
+struct robot_data data; //储存机器人数据
+  /*
+  int num;           // 机器人编号
+  double x, y;       // 机器人位置
+  int table;         // 机器人所处的工作台ID -1表示当前没有处于任何工作台周围 从0开始
+  int object;        // 携带物品种类 0表示未携带
+  double time_value; // 时间价值系数
+  double col_value;  // 碰撞价值系数
+  double ang_speed   // 角速度
+      double speed_x,
+      speed_y;   // 线速度
+  double toward; // 朝向 与平面直角坐标系的单位圆射线方向相同 toward=0时向右
+  */
+
+
+
+};
