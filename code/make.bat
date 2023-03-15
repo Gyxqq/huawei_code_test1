69ee88bc1f6e0aca53e1a53fc7e39efc8533e2b5
Gyxqq <108503818+Gyxqq@users.noreply.github.com>
2023-03-15 11:16:40 +0800

    gyx-151117 增加构建脚本make.bat

--- code/make.bat
+++ code/make.bat
@@ -1,5 +1,12 @@
 g++ c_map.cpp -c -o map.o
+g++ c_map1.cpp -c -o map1.o
 g++ c_robot.cpp -c -o robot.o
 g++ input.cpp -c -o input.o
-g++ out_put_process -c -o out_put_process.o
+g++ main.cpp -c -o main.o
+g++ output_process.cpp -c -o  output_process.o
 g++ output.cpp -c -o output.o
+g++ process_control.cpp -c -o process_control.o
+g++ robot_table_control.cpp -c -o robot_table_control.o
+g++ value_hash.cpp -c -o hash.o
+g++ main.o map.o map1.o robot.o input.o output_process.o output.o process_control.o robot_table_control.o hash.o -o main.exe
+del *.o
