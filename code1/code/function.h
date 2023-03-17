5d19ed87893d167b9b8210182271e5c5e0c8d36f
Your Name <you@example.com>
2023-03-18 01:05:31 +0800

    jzt-180105 修改value_hash传入参数

--- code1/code/function.h
+++ code1/code/function.h
@@ -5,7 +5,7 @@ bool input(int& now_frame, int& now_money, table* table_in_map, int* table_num, 
 
 bool output(output_command* out_put_command); // 对储存的需要输出的数据进行输出
 
-double value_hash(map now_map, robot now_bot, int ori, int des);
+double value_hash(map now_map, robot now_bot, int ori, int des,int now_frame);
 // 规划方案哈希值计算，哈希值越大规划方案的利润越大
 
 bool robot_table_control(map& now_map, robot now_bot[]); // 对未接受调度的机器人进行调度，方式为在函数中对机器人的control_flag,ori和des进行设置,生成调度方案并利用value_hash选择最优方案
